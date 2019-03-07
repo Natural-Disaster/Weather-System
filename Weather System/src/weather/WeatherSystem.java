a9c1f9d8b295e89f3eac82d65d4737f742860cc5
CMPAKNOW <CMPAKNOW@CMPL03.cms.livjm.ac.uk>
2019-03-07 13:37:27 +0000

    hi

--- Weather System/src/weather/WeatherSystem.java
+++ Weather System/src/weather/WeatherSystem.java
@@ -16,7 +16,7 @@ public class WeatherSystem {
 	public static void main(String[] args) throws FileNotFoundException {
 		while (menuLoop) {
 
-			///abbie smells
+	
 			
 			switch(displayMainMenu()) {
 			case "1":
